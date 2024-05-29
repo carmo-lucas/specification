eee247dcddc29a70a2e4ecec8130c4f78febac89
Lucas Carmo <msclucaslima@gmail.com>
2024-05-28 21:29:11 -0300

    New model for recording methods of analysis

---
+++ models/method.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api, _
+
+
+class SpecificationMethod(models.Model):
+    _name = "spec.method"
+    _description = "Test method for give parameter"
+
+    parameter_id = fields.Many2one("spec.parameter")
+
+    name = fields.Text(string="Method Name")
+    description = fields.Text(string="Description")
+    reference = fields.Text(string="Bibliography reference")
