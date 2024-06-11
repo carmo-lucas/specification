05e60432fa8e18ffb11751989c75660fba733e76
Lucas Carmo <msclucaslima@gmail.com>
2024-06-11 20:42:19 -0300

    write_to_thread method is now working as expected

--- wizard/validate_spec_sheet.py
+++ wizard/validate_spec_sheet.py
@@ -1,13 +1,26 @@
-from odoo import fields, models
+from odoo import fields, models, api
+
 
 class ValidateSpecificationSheet(models.TransientModel):
     _name = "validate.spec.sheet"
     _description = "Validation of specification sheet"
 
-    description = fields.Char("Description", required = True)
-    password = fields.Char("password", required = True)
-# TODO: function not working as expected
-    def write_to_thread(self):
-        message = f"Name:{self.name} validated this specification using the password with the description: {self.description}"
-        self.message_post(body=message)
-        return True
+    specification_sheet_id = fields.Many2one("spec.sheet")
+    comment = fields.Text("Comment", required=True)
+    password = fields.Char("Password", required=True)
+
+    # TODO: function not working as expected
+    @api.model
+    def write_to_thread(self, fields):
+        active_id = self.env.context.get("active_id")
+        if active_id:
+            self.env["spec.sheet"].browse(active_id).message_post(
+                body="message working"
+            )
+            return {"type": "ir.actions.act_window_close"}
+
+    @api.model
+    def default_get(self, fields):
+        res = super(ValidateSpecificationSheet, self).default_get(fields)
+        res["specification_sheet_id"] = self.env.context.get("active_id")
+        return res
