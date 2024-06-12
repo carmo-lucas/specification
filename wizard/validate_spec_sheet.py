9b2edfee1d7fca7120cf7e5da5ebc8b6b0c7b702
Lucas Carmo <msclucaslima@gmail.com>
2024-06-11 21:41:30 -0300

    write_to_thread method working as expected

--- wizard/validate_spec_sheet.py
+++ wizard/validate_spec_sheet.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from markupsafe import Markup
 
 
 class ValidateSpecificationSheet(models.TransientModel):
@@ -9,18 +10,20 @@ class ValidateSpecificationSheet(models.TransientModel):
     comment = fields.Text("Comment", required=True)
     password = fields.Char("Password", required=True)
 
-    # TODO: function not working as expected
-    @api.model
-    def write_to_thread(self, fields):
-        active_id = self.env.context.get("active_id")
-        if active_id:
-            self.env["spec.sheet"].browse(active_id).message_post(
-                body="message working"
-            )
-            return {"type": "ir.actions.act_window_close"}
+
 
     @api.model
     def default_get(self, fields):
         res = super(ValidateSpecificationSheet, self).default_get(fields)
         res["specification_sheet_id"] = self.env.context.get("active_id")
         return res
+
+    # TODO: comment returning as False
+
+    def write_to_thread(self):
+        active_id = self.env.context.get("active_id")
+        if active_id:
+            self.env["spec.sheet"].browse(active_id).message_post(
+                body=Markup(f"<b>{self.env.user.name}</b> has validated this specification with password.<br><b>Comment:</b> <i>'{self.comment}</i>'")
+            )
+            return {"type": "ir.actions.act_window_close"}
